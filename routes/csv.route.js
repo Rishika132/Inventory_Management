cedff1b007ebfacb63049167087acab2d17fe7ef
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-10 16:57:19 +0530

    first commit

--- routes/csv.route.js
+++ routes/csv.route.js
@@ -3,12 +3,12 @@ const multer = require('multer');
 const { auth } = require ("../middleware/auth.js");
 const { handleCSVUpload } = require('../controller/csv.controller');
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp' });
 
 const router = express.Router();
 
 //http://localhost:3000/api/upload-csv?csv
 
-router.post('/upload-csv', upload.single('csv'),auth, handleCSVUpload);
+router.post('/upload-csv',auth, upload.single('csv'), handleCSVUpload);
 
 module.exports = router;
