8c5f74a67047862d529101778fa15831a7007c9b
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-11 10:17:07 +0530

    first commit

--- routes/export.route.js
+++ routes/export.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { exportSyncData } = require('../controller/exportSyncData.controller');
-// const { auth } = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 
-router.get('/export-sync', exportSyncData);
+router.get('/export-sync',auth, exportSyncData);
 
 module.exports = router;
