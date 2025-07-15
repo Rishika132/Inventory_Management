47df4a420be9c43565a87e531e7fc05f0ae45ba4
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-15 12:09:03 +0530

    first commit

--- routes/pagination.route.js
+++ routes/pagination.route.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { auth } = require ("../middleware/auth.js");
 const router = express.Router();
 const { getSyncData } = require("../controller/pagination.controller");
+const {getOrder} = require("../controller/order.controller.js");
 
 //http://localhost:3000/api/syncpage?page=3
 
 router.get("/syncpage",auth, getSyncData); 
+
+router.get("/orderpage",getOrder);
 
 module.exports = router;
