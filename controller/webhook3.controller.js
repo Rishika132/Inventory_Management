59e22f2b3dd1cc95d87d192b28583a10dacdbd85
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-15 12:00:43 +0530

    first commit

--- controller/webhook3.controller.js
+++ controller/webhook3.controller.js
@@ -41,45 +41,7 @@ const Webhook3 = async (req, res) => {
   }
 };
 
-const Webhook4 = async (req, res) => {
-  try {
-    const order = req.body;
 
-    const orderId = order.id;
-    const storeName = req.headers["x-shopify-shop-domain"] || null;
-
-    const lineItems = order.line_items || [];
-
-    const inserted = [];
-
-    for (const item of lineItems) {
-      const sku = item.sku?.trim();
-      const quantity = item.quantity;
-      const variant_title = item.title;
-
-      if (!sku || !quantity || !variant_title || !orderId) continue;
-
-      const saved = await Order.findOneAndUpdate(
-        { sku },
-        {
-          sku,
-          quantity,
-          variant_title,
-          order_id: String(orderId),
-          store_name: storeName,
-        },
-        { upsert: true, new: true, setDefaultsOnInsert: true }
-      );
-
-      inserted.push(saved);
-    }
-
-    return res.status(200).json({ message: "✅ Order synced", inserted });
-  } catch (err) {
-    console.error("❌ Webhook error:", err.message);
-    return res.status(500).json({ error: "Failed to handle webhook" });
-  }
-};
 
 // // Order-delete
 // const Sync = require("../model/sync.model");
@@ -140,4 +102,4 @@ const Webhook4 = async (req, res) => {
 //   }
 // };
 
-module.exports = { Webhook3 , Webhook4 };
+module.exports = { Webhook3 };
--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const {Webhook} = require("../controller/webhook.controller");
 const {Webhook2} = require("../controller/webhook2.controller");
-const {Webhook3 , Webhook4} = require("../controller/webhook3.controller");
-// const {Webhook4 } = require("../controller/webhook4.controller");
-const {Webhook5} = require("../controller/webhook5.controller")
+const {Webhook3 } = require("../controller/webhook3.controller");
+const {Webhook4 } = require("../controller/webhook4.controller");
+// const {Webhook5} = require("../controller/webhook5.controller")
 
 const router = express.Router();
 
@@ -14,9 +14,9 @@ router.post("/order",Webhook);
 router.post("/order2",Webhook2);
 
 router.post("/order-creation" , Webhook3);
-// router.post("/order-creation2" , Webhook4);
+router.post("/order-creation2" , Webhook4);
 
-router.post("/order-delete" , Webhook5);
+// router.post("/order-delete" , Webhook5);
 // router.post("/order-delete2");
 
 module.exports = router;
